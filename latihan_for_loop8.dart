e4f801f47b0173ef8e252c02857ebdd21535ceb4
ipls <andresaefullohxx@gmail.com>
2023-01-11 16:08:36 +0700

    revisi latihan 8

--- latihan_for_loop8.dart
+++ latihan_for_loop8.dart
@@ -3,9 +3,11 @@ import 'dart:io';
 var batasAkhir = 7;
 void main(List<String> args) {
   for (var i = 1; i <= batasAkhir; i++) {
+    var k = '';
     for (var j = 1; j <= i; j++) {
-      print('$j');
+      k += j.toString();
     }
+    print(k);
   }
 }
 
